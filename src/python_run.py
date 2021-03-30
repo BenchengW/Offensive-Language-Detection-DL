6ef648190ebba49a72fb8868b78d7cf7c07a84dc
BenchengW <36852259+BenchengW@users.noreply.github.com>
2021-03-29 21:44:22 -0400

    Update python_run.py

--- src/python_run.py
+++ src/python_run.py
@@ -5,3 +5,20 @@
 import os
 import sys
 os.chdir('Hate_Speech_Detection_MMAI_894_DL/src')
+
+#################################################
+# Load Albert pretrain model
+################################################
+from main import Albert_pretrain
+Pretrain = Albert_pretrain()
+Pretrain.load_albert()
+Pretrain.predict("I like it")
+
+#################################################
+# Load Albert model and train on your own data
+################################################
+from main import *
+New_data = load_data()
+Albert_model = Albert(New_data, 50, 2)    #50 batch size and 2 epoch
+Albert_model.fit_albert()
+Albert_model.predict("I like it")
